710362b38a55cfd19626d1bee8d5e0f597dde509
Ikaromm <ikaromm@hotmail.com>
2023-11-01 11:21:27 -0300

    Add Fiis

--- stonks_analytics_scraper/scraper/shape/fiis.py
+++ stonks_analytics_scraper/scraper/shape/fiis.py
@@ -54,16 +54,39 @@ FII_SHAPE = [
     {
         "name": "dy5y",
         "path": '//*[@id="dividends-section"]/div[1]/div[1]/span[2]/text()',
-        "type": DataType.NUMERIC,
+        "type": DataType.NUMERIC
     },
     {
         "name": "vacancy",
         "path": '//*[@id="table-indicators"]/div[9]/div[2]/div/span',
-        "type": DataType.NUMERIC,
+        "type": DataType.NUMERIC
     },
     {
-        "name": "asset_value",
+        "name": 'asset_value',
         "path": '//*[@id="table-indicators"]/div[14]/div[2]/div/span',
-        "type": DataType.NUMERIC,
+        "type": DataType.NUMERIC
     },
+    {
+        "name": "last_income",
+        "path": '//*[@id="table-indicators"]/div[15]/div[2]/div/span',
+        "type": DataType.NUMERIC
+    },
+    # {
+    #     "name": "properties",
+    #     "path": 
+    #             try:
+    #                 num_rows = 15
+
+    #                 imv_values = []
+
+    #                 for i in range(1, num_rows + 1):
+    #                     xpath = (
+    #                         f'//*[@id="properties-index-table"]/tbody/tr[{i}]/td[2]/span'
+    #                     )
+    #                     imv = extract_numeric_from_xpath(xpath, browser)
+    #                     imv_values.append(imv)
+    #             except:
+    #                 imv_values = sum(imv_values)
+    #     "type": DataType.NUMERIC        
+    # }
 ]
